2366ab39ab4656c04de908d2e088d1a70136b73a
toba-madamori <tobajamezmadamori@gmail.com>
2021-01-05 09:28:00 -0800

    Returning blog post title in admin

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -9,5 +9,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     body = models.TextField()
 
+    def __str__(self):
+        return(self.title)
+
     def get_absolute_url(self):
         return reverse('home')
